8e448e9986aef567de9180ebeba20625a618348a
alishalawani <alishalawani@gmail.com>
2022-03-13 03:33:23 -0500

    created sample header

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	content: [
+		'./pages/**/*.tsx',
+		'./components/**/*.tsx',
+		// For the best performance and to avoid false positives,
+		// be as specific as possible with your content configuration.
+	],
+};
